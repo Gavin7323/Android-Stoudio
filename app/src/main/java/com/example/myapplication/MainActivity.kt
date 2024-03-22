35b00c95dcc9230d5b48d3d893a7c914fc691067
Gavin Wilson <wilsogav000@warren.k12.in.us>
2024-03-22 10:29:54 -0400

    Spring Break!!!!!!!!!!

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,18 +2,37 @@
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Random
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+    private fun rollDice() {
+        TODO("Not yet implemented")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById<>(R.id.roll_button)
         rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+
+        private fun rollDice() {
+            val resultText: TextView = findViewById(R.id.result_text)
+
+            val randomInt = Random().nextInt(6) + 1
+
+            resultText.text = randomInt.toString()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.roll_button)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
